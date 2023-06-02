e50efc7a90548ae82cb967d1388f9dc70a5b2863
Jerp <jerpdoesgames@gmail.com>
2023-06-02 02:50:17 -0500

    Relatively unobtrusive stream embed
    Setting up the same embed as the one I used for the Elden Ring tools.  It can be quickly, permanently disabled in a way that prevents the stream from even attempting to load.
    
    Just in case people want to support me w/some exposure - I don't get paid to write these things, after all.

---
+++ QuickReference/script/headerStream.js
@@ -0,0 +1,47 @@
+const streamElementContents = `
+    <iframe id="twitchPlayer"
+    src="https://player.twitch.tv/?channel=jerp&muted=true&parent=jerp.tv"
+
+    allowfullscreen>
+    </iframe>
+    <p>Leaving this stream open helps support Jerp, but you can <a href="#" onclick="headerStreamClose();" id="headerStreamClose">close it</a> if you'd like.</p>
+`;
+
+let headerClosed = localStorage.getItem("jerpHeaderStreamClosed");
+
+function headerStreamOpen()
+{
+    headerClosed = 0;
+    localStorage.setItem("jerpHeaderStreamClosed", "0");
+    updateHeaderVis();
+}
+
+function headerStreamClose()
+{
+    headerClosed = 1;
+    localStorage.setItem("jerpHeaderStreamClosed", "1");
+    updateHeaderVis();
+}
+
+function updateHeaderVis()
+{
+    let headerStream = document.getElementById("headerStream");
+    let headerStreamOpen = document.getElementById("headerStreamOpen");
+    if (headerClosed === null || headerClosed == "0")
+    {
+        headerStream.style.display = "flex";
+        headerStreamOpen.style.display = "none";
+        headerStream.innerHTML = streamElementContents;
+    }
+    else
+    {
+        headerStream.style.display = "none";
+        headerStreamOpen.style.display = "block";
+        headerStream.innerHTML = "";
+    }
+}
+
+function initializeHeaderStream()
+{
+    updateHeaderVis();
+}
